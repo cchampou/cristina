b12ff04afb1e5d6b7072b4f810f32e9f9c9d612d
cchampou <clement@champouillon.com>
2024-06-04 23:04:13 +0200

    feat: add cursor fullscreen CTA

--- frontend/src/components/Presentation/index.tsx
+++ frontend/src/components/Presentation/index.tsx
@@ -1,38 +1,38 @@
 import React, {useMemo, useRef} from 'react';
 
-import type { Collection } from '../../services/api';
-import { getUploadURL } from '../../utils/uploads';
+import type {Collection} from '../../services/api';
+import {getUploadURL} from '../../utils/uploads';
 
 import './index.css';
 
 type PresentationProps = {
-  collection: Collection;
-  selectedPhotoIndex: number;
+    collection: Collection;
+    selectedPhotoIndex: number;
 };
 
-function Presentation({ collection, selectedPhotoIndex }: PresentationProps) {
-  const presentationImageRef = useRef<HTMLImageElement>(null);
+function Presentation({collection, selectedPhotoIndex}: PresentationProps) {
+    const presentationImageRef = useRef<HTMLImageElement>(null);
 
-  const requestFullScreen = () => {
-    if (!presentationImageRef.current) return;
-    presentationImageRef.current.requestFullscreen();
-  }
+    const requestFullScreen = () => {
+        if (!presentationImageRef.current) return;
+        presentationImageRef.current.requestFullscreen();
+    }
 
-  const currentPhoto = useMemo(() => collection?.attributes.photos[selectedPhotoIndex],
-    [collection, selectedPhotoIndex]);
+    const currentPhoto = useMemo(() => collection?.attributes.photos[selectedPhotoIndex],
+        [collection, selectedPhotoIndex]);
     console.log(currentPhoto);
 
-  const currentPhotoUrl = useMemo(() => currentPhoto ?
-      getUploadURL(currentPhoto.file.data.attributes.formats.large.url) : undefined,
-    [currentPhoto]);
+    const currentPhotoUrl = useMemo(() => currentPhoto ?
+            getUploadURL(currentPhoto.file.data.attributes.formats.large.url) : undefined,
+        [currentPhoto]);
 
-  const currentPhotoDescription = useMemo(
-    () => currentPhoto ? currentPhoto.description : undefined
-    , [currentPhoto]);
+    const currentPhotoDescription = useMemo(
+        () => currentPhoto ? currentPhoto.description : undefined
+        , [currentPhoto]);
 
-  const currentPhotoLocation = useMemo(
-      () => currentPhoto ? currentPhoto.location : undefined
-      , [currentPhoto]);
+    const currentPhotoLocation = useMemo(
+        () => currentPhoto ? currentPhoto.location : undefined
+        , [currentPhoto]);
 
     const currentPhotoCamera = useMemo(
         () => currentPhoto ? currentPhoto.camera : undefined
@@ -42,24 +42,25 @@ function Presentation({ collection, selectedPhotoIndex }: PresentationProps) {
         () => currentPhoto ? currentPhoto.date : undefined
         , [currentPhoto]);
 
-  return (
-    <section className="presentation-section">
-      <h1>{collection?.attributes.title}</h1>
-      <p>{collection?.attributes.summary}</p>
-      <div className="presentation-current-photo">
-        <img ref={presentationImageRef} src={currentPhotoUrl} alt={currentPhotoDescription} onClick={requestFullScreen} />
-        <div>
-          <p>{currentPhotoDescription}</p>
-          <hr />
-          <ul>
-            <li><strong>Lieu:</strong> {currentPhotoLocation}</li>
-            <li><strong>Appareil photo:</strong> {currentPhotoCamera}</li>
-            <li><strong>Date:</strong> {currentPhotoDate}</li>
-          </ul>
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className="presentation-section">
+            <h1>{collection?.attributes.title}</h1>
+            <p>{collection?.attributes.summary}</p>
+            <div className="presentation-current-photo">
+                <img ref={presentationImageRef} src={currentPhotoUrl} alt={currentPhotoDescription}
+                     onClick={requestFullScreen} title="Cliquez pour agrandir" style={{cursor: "pointer"}}/>
+                <div>
+                    <p>{currentPhotoDescription}</p>
+                    <hr/>
+                    <ul>
+                        <li><strong>Lieu:</strong> {currentPhotoLocation}</li>
+                        <li><strong>Appareil photo:</strong> {currentPhotoCamera}</li>
+                        <li><strong>Date:</strong> {currentPhotoDate}</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Presentation;
