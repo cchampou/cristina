d4d1bef28028c055fb27458f4a10e19d9c8cb4d0
Clement Champouillon <clement@champouillon.com>
2024-11-17 22:43:58 +0100

    fix(front): links in ref

--- frontend/src/components/ReferenceCard/index.tsx
+++ frontend/src/components/ReferenceCard/index.tsx
@@ -12,9 +12,9 @@ function ReferenceCard({ link, title, media, date }: Props) {
     <>
       <a href={link} target="_blank">
         {title},&nbsp;
-        <strong>{media}</strong>,&nbsp;
-        {date}
       </a>
+      <strong>{media}</strong>,&nbsp;
+      {date}
       <br/>
     </>
   );
