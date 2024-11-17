10a79e72a282c1cc5b4c14195e918ef473b8fe1e
Clement Champouillon <clement@champouillon.com>
2024-11-17 21:21:40 +0100

    feat: mobile menu

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import routes from '../../router';
 import './index.css';
@@ -8,16 +8,37 @@ type Props = {
 }
 
 function Header({ immersive }: Props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleResize = () => {
+    if (window.innerWidth > 768) {
+      setIsOpen(true);
+    } else {
+      setIsOpen(false);
+    }
+  }
+
+  useEffect(() => {
+    handleResize();
+    const observer = new ResizeObserver(() => {
+      handleResize();
+    });
+
+    observer.observe(document.body);
+  }, []);
 
   return <nav id="header" style={{
     position: immersive ? 'absolute' : 'relative',
     color: immersive ? 'white' : 'black',
     textShadow: immersive ? '0 2px 4px gray' : 'none',
+    backgroundColor: immersive ? 'transparent': 'white',
   }}>
-    <NavLink to={routes.homepage.path}>Home</NavLink>
-    <NavLink to={routes.photography.path}>Photography</NavLink>
-    <NavLink to={routes.journalism.path}>Journalism</NavLink>
-    <NavLink to={routes.contact.path}>Contact</NavLink>
+    <button onClick={() => setIsOpen(prev => !prev)} style={{ color: immersive ? 'white' : 'black' }}>=</button>
+    {isOpen && (<><NavLink to={routes.homepage.path}>Home</NavLink>
+      <NavLink to={routes.photography.path}>Photography</NavLink>
+      <NavLink to={routes.journalism.path}>Journalism</NavLink>
+      <NavLink to={routes.contact.path}>Contact</NavLink>
+    </>)}
   </nav>
 }
 
