cecbcd70cd5827585c4af936aef8832e831e71fd
Clement Champouillon <clement@champouillon.com>
2024-11-19 22:11:43 +0100

    feat(front): add description to photo

--- frontend/src/components/PhotoGallery/index.tsx
+++ frontend/src/components/PhotoGallery/index.tsx
@@ -1,6 +1,6 @@
 import "react-image-gallery/styles/css/image-gallery.css";
-import React, { useEffect } from 'react';
-import ImageGallery from 'react-image-gallery';
+import React from 'react';
+import ImageGallery, { ReactImageGalleryItem } from 'react-image-gallery';
 
 import { Photo } from '../../services/api';
 import { getUploadURL } from '../../utils/uploads';
@@ -13,10 +13,11 @@ type PhotoGalleryProps = {
 
 function PhotoGallery({ photos }: PhotoGalleryProps) {
 
-  const images = photos.map(photo => ({
+  const images: ReactImageGalleryItem[] = photos.map(photo => ({
     original: getUploadURL(photo.file.formats.large.url),
     thumbnail: getUploadURL(photo.file.formats.thumbnail.url),
     originalHeight: window.innerHeight * 0.6,
+    description: photo.description,
   }));
 
   return (
