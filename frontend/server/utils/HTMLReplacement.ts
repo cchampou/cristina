eda16d345e11f495971a46c18c5aa5cbb8199ba6
Clement Champouillon <clement@champouillon.com>
2024-12-07 17:19:38 +0100

    feat(SEO): add alternates

--- frontend/server/utils/HTMLReplacement.ts
+++ frontend/server/utils/HTMLReplacement.ts
@@ -1,15 +1,17 @@
 import { findRouteByPath } from '../../src/router';
+import { supportedLocales } from '../../app.config';
 
 const DEFAULT_TITLE = 'Cristina Coellen';
 
 export function HTMLReplacement(template: string, serverHtml: string, path: string, lang: string) {
   const htmlWithoutTitle = template.replace(`<!--ssr-outlet-->`, serverHtml);
   const htmlWithLang = htmlWithoutTitle.replace('<!--lang-->', lang);
+  const htmlWithAlternate = htmlWithLang.replace('<!--alternate-->', supportedLocales.filter(lng => lng !== lang).map(locale => `<link rel="alternate" hreflang="${locale}" href="https://cristinacoellen.com${path}?lang=${locale}">`).join(''));
   const routeConfig = findRouteByPath(path);
   if (!routeConfig) {
-    return htmlWithLang;
+    return htmlWithAlternate;
   }
-  const withTitle = replaceTitle(htmlWithLang, routeConfig.title || DEFAULT_TITLE);
+  const withTitle = replaceTitle(htmlWithAlternate, routeConfig.title || DEFAULT_TITLE);
   return withTitle.replace('<!--meta-description-->', `<meta name="description" content="${routeConfig.getDescription()}">`);
 }
 
