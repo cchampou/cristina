c9a84c2d544845e6febf3d32a23aca893777506d
Clement Champouillon <clement@champouillon.com>
2024-12-08 19:15:40 +0100

    feat(front): echo version in the footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 import './index.css';
 
+function getVersion(): string | undefined {
+  return import.meta.env.VITE_VERSION;
+}
+
 function Footer() {
   const { t } = useTranslation();
+  const version = useMemo(getVersion, []);
   return (
     <footer>
       <p>Copyright © Cristina Coellen 2024</p>
+      {version ? <p>{version}</p> : null}
       <Link to="/legal">{t('legal notice')}</Link>
     </footer>
   );
